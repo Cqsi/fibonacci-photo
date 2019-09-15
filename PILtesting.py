2286e64e35b53e8bf9db0d7c9bca6f4716b8b822
Cqsi <casimirr04@gmail.com>
2019-09-15 10:55:25 +0300

    k

--- PILtesting.py
+++ PILtesting.py
@@ -9,9 +9,6 @@ fibonacci_limit = 4
 for i in range(fibonacci_limit):
     fibonacci_photos.append((fibonacci_photos[i][0] + fibonacci_photos[i+1][0], fibonacci_photos[i][0] + fibonacci_photos[i+1][0]))
 
-area_of_image = sum(i[0]*i[0] for i in fibonacci_photos)
-new_image_size = (fibonacci_photos[-1][0], area_of_image/fibonacci_photos[-1][0])
-
 image1 = Image.open(path + name)
 file_name, ext = os.path.splitext(name)
 
@@ -19,14 +16,5 @@ for i in range(len(fibonacci_photos)):
     image1.resize(fibonacci_photos[i]).save((path + "converted\\" + "{}_" + str(fibonacci_photos[i][0]) + "{}").format(file_name, ext))
     image1 = Image.open(path + name)
 
-#width, height = image1.size
-#print(str(width) + " " + str(height))
-
-#for f in os.listdir(path):
-#    if f.endswith(".jpg"):
-#        i = Image.open(path + f)
-#        fn, fext = os.path.splitext(f)
-        
-#        print(fn)
-
-#image1.show()
+area_of_image = sum(i[0]*i[0] for i in fibonacci_photos)
+new_image_size = (fibonacci_photos[-1][0], area_of_image/fibonacci_photos[-1][0])
