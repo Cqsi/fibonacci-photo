9999bb48ddaaa034098e9cbd13a1f4d450a77f81
Cqsi <casimirr04@gmail.com>
2019-09-15 10:54:04 +0300

    k

--- PILtesting.py
+++ PILtesting.py
@@ -1,14 +1,16 @@
 from PIL import Image
 import os
 
+name = "square.png"
+path = "C:\\Users\\Capsimir\\Desktop\\VisualStudio\\PythonProjects\\PIL\\"
 fibonacci_photos = [(100, 100), (100, 100)]
 fibonacci_limit = 4
 
 for i in range(fibonacci_limit):
     fibonacci_photos.append((fibonacci_photos[i][0] + fibonacci_photos[i+1][0], fibonacci_photos[i][0] + fibonacci_photos[i+1][0]))
 
-name = "square.png"
-path = "C:\\Users\\Capsimir\\Desktop\\VisualStudio\\PythonProjects\\PIL\\"
+area_of_image = sum(i[0]*i[0] for i in fibonacci_photos)
+new_image_size = (fibonacci_photos[-1][0], area_of_image/fibonacci_photos[-1][0])
 
 image1 = Image.open(path + name)
 file_name, ext = os.path.splitext(name)
