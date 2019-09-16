332b5a53e3912e817ffcc638c9873e637c71060c
Cqsi <casimirr04@gmail.com>
2019-09-16 13:23:20 +0300

    added an image dict

--- PILtesting.py
+++ PILtesting.py
@@ -21,6 +21,7 @@ area_of_image = sum(i[0]*i[0] for i in fibonacci_photos)
 new_image_size = (fibonacci_photos[-1][0], area_of_image//fibonacci_photos[-1][0])
 
 new_image = Image.new("RGB", new_image_size)
+#new_image.show()
 
 # 1 = south
 # 2 = east
@@ -28,3 +29,16 @@ new_image = Image.new("RGB", new_image_size)
 # 4 = west
 
 direction = 1
+image_dict = {}
+
+for i in os.listdir(path + "converted\\"):
+    image_name, image_ext = os.path.splitext(i)
+    image_dict[image_name + image_ext] = int(image_name)
+
+print(image_dict)
+
+#for i in range(len(fibonacci_photos)-1, -1, -1):
+
+    
+   
+    
